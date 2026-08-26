319d9a2d3d0a8f6863d55c8d09fa6d2fd0978959
likefack <codeswitcher41@gmail.com>
2026-08-27 07:38:07 +0900

    Approved Host fail closed gateを初期化

--- src/windows_local_mcp/__init__.py
+++ src/windows_local_mcp/__init__.py
@@ -1,1 +1,8 @@
+from .approved_host_policy import (
+    install_approved_host_fail_closed_gate as _install_approved_host_fail_closed_gate,
+)
+
+_install_approved_host_fail_closed_gate()
+del _install_approved_host_fail_closed_gate
+
 __version__ = "0.6.0"
